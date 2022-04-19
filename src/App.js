05d1ca3471628db869668d0e8fd391ad5cbeb318
kmahboob <kmahboob@securitycompass.com>
2022-04-19 15:42:40 -0400

    added particle background

--- src/App.js
+++ src/App.js
@@ -5,17 +5,249 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const particleOptions = {
+    
+  fullScreen: {
+    enable: true,
+    zIndex: 0
+  },
+  particles: {
+    number: {
+      value: 200,
+      limit: 300,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: "images/github.svg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.5,
+        sync: false
+      }
+    },
+    size: {
+      value: 30,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 10,
+        size_min: 10,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 100,
+      color: "#ffffff",
+      opacity: 1,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onHover: {
+        enable: true,
+        mode: "bubble",
+        parallax: {
+          enable: false,
+          force: 60,
+          smooth: 10
+        }
+      },
+      onClick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        lineLinked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 100,
+        duration: 2,
+        opacity: 1,
+        speed: 2
+      },
+      repulse: {
+        distance: 200
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  backgroundMask: {
+    enable: true,
+    cover: {
+      color: {
+        value: {
+          r: 0,
+          g: 0,
+          b: 0
+        }
+      }
+    }
+  },
+  retina_detect: true,
+  fps_limit: 60,
+  background: {
+    image: "url('https://particles.js.org/images/background3.jpg')"
+  }
+}
+
+const particleOptions2 = {
+  // background: {
+  //   color: {
+  //     value: "#0d47a1",
+  //   },
+  // },
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: false,
+        mode: "push",
+      },
+      onHover: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: false,
+    },
+    modes: {
+      push: {
+        quantity: 2,
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+      // opacity:0.5,
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true
+
+}
 
 class App extends Component {
 
   render(){
+
+    async function particlesInit  (main) {
+      console.log(main);
+      // let tsParticles = main;
+      // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      await loadFull(main);
+    }
+
+    function particlesLoaded (container){
+      console.log(container);
+    };
+  
+
     return (
       <div className="App">
+        <Particles className="tsparticles" params={particleOptions2} init={particlesInit} loaded={particlesLoaded} />
+
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm />
         {/*<FaceRecognition /> */}
+
       </div>
     );
   }
