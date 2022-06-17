a493c9ae9c7a23e724eb9d1b9faf47b245d0138b
Khizer22 <khizer405@gmail.com>
2022-06-17 06:51:25 -0400

    centered upload/detect url text

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -29,11 +29,11 @@ const ImageLinkForm = ({onInputChange , onDetectURL,onFileUpload,onFileChange, g
             <div className='center form pa4 br3 shadow-5'>
                 <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}  />
 
-                <label htmlFor="urlSelect" className='w-20 grow f5 link ph1 pv2 dib white bg-light-purple'>Detect URL</label>
+                <label htmlFor="urlSelect" className='w-20 grow f5 link ph1 pv2 white bg-light-purple'>Detect URL</label>
                 <button id='urlSelect' onClick={onDetectURL} style={{display:"none"}}>Detect URL</button>
 
                 <input type='file' id='fileSelect' accept="image/png, image/gif, image/jpeg" style={{display:"none"}} onChange={onFileChange}/>
-                <label htmlFor="fileSelect" className='w-20 grow f5 link ph1 pv2 dib white bg-light-purple'>Upload File</label>
+                <label htmlFor="fileSelect" className='w-20 grow f5 link ph1 pv2 white bg-light-purple'>Upload File</label>
                 {/* <button onClick={onFileUpload} className='w-20 grow f5 link ph1 pv2 dib white bg-light-purple'>UPLOAD</button> */}
             </div>
         </div>
